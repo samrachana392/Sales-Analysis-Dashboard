ce89fc2d572f663da521523af873bd061c04590d
samrachana392 <samrachana302@gmail.com>
2025-12-30 13:16:07 +0545

    Edited some code

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.max_rows', None)     # Show all rows
+pd.set_option('display.float_format', '{:.2f}'.format)  # Round floats to 2 decimals
+
+
 df = pd.read_csv("Sales.csv")
-#print(df)
+print("\nDisplaying all data: \n")
+print(df)
 
 df['Revenue']=df['Quantity']*df['Rate']
-print(" Data with revenue: ")
-#print(df)
+print(" \nData with revenue: \n")
+print(df)
+
 
 total_revenue = df['Revenue'].sum()
 print(f"\nTotal Revenue from all prducts: {total_revenue}\n")
 
-print("Sorting all products by revenue ")
+
+print("\nSorting all products by revenue \n")
 products = df.sort_values(by='Revenue', ascending=False) # sorts all the products by revenue in decending order
 print(products[['Product','Revenue']]) # in descending form prints product and revenue
-print("\n")
 
-print("The Top product that got the highest revenue is: ")
+
+print("\nThe Top product that got the highest revenue is: \n")
 top_product=products.iloc[0]
 print("Product: ",top_product['Product'])
 print("Revenue: ", top_product['Revenue'])
@@ -26,15 +35,15 @@ print("Revenue: ", top_product['Revenue'])
 
 each_unit_revenue=df.groupby('Unit')['Revenue'].sum() # this groups the df by the unit type eg: kg, pice, packet and does the sum of revenue in each group
 unit_revenue=each_unit_revenue.sort_values( ascending=False) # this makes the above series data in descending form 
-print("\nRevenue per unit type:")
+print("\n\nRevenue per unit type:\n")
 print(unit_revenue) # prints the unit and the total revenue
 
+
 each_type_revenue=df.groupby('Type')['Revenue'].sum().sort_values(ascending=False)
-print("\n Revenues Per type:")
+print("\nRevenues Per type:\n")
 print(each_type_revenue) # here we can group item by type and see on only one column i.e. revenue only and we can play only with it
 #We grouped by 'Type', We summed the 'Revenue' column, so each_type_revenue.name == 'Revenue'
 # i.e the name of this each_type_revenue series is revenue
-
 
 
 # but here in .agg(), we group item by type same as above but we can see different columns we want in the same function and we can play with any columns we want in the same function
@@ -43,18 +52,20 @@ type_summary = df.groupby('Type').agg(
     Average_Rate = ('Rate','sum'),
     Total_Revenue = ('Revenue','sum')       
     )
+type_summary['Revenue_percent']=(type_summary['Total_Revenue']/total_revenue)*100
 type_summary=type_summary.sort_values(by='Total_Revenue', ascending = False)
-print("\nSales Summary by Type:")
+print("\n\nSales Summary by Type:\n")
 print(type_summary)
 
 
-# Top 10 products by revenue
+# Top 10 products by revenue(barplot)
 plt.figure(figsize=(10,6)) # inside this frame of matplotlib sns barplot s drawn
-sns.barplot(x='Revenue', y='Product', data=products.head(10), palette='Set1')
-plt.title("Top 10 products by Revenue")
+sns.barplot(x='Revenue', y='Product', data=products.head(10), palette='Set3')
+plt.title("\nTop 10 products by Revenue\n")
 plt.xlabel("Revenue")
 plt.ylabel("Product")
 plt.show()
+
 
 # Revenue by Type (pie chart)
 plt.figure(figsize=(6,6))
